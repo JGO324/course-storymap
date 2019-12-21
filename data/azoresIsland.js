09b3b26f2da1b4600a5869d31b1505913353b240
joel <joel_congo@hotmail.com>
2019-12-21 18:07:38 -0400

    more attribute added

--- data/azoresIsland.js
+++ data/azoresIsland.js
@@ -6,7 +6,9 @@ var azores={
       "properties": {
         "name": "Ilha de Sao Miguel",
         "location": "Lagoa do Santiago",
-        "pic": "https://byacores.com/wp-content/uploads/2017/10/DSC08576.jpg"
+        "pic": "https://byacores.com/wp-content/uploads/2017/10/DSC08576.jpg",
+        "caption":"Lagoa Verde, Sete Cidades e Lagoa de Santiago is a 17.7 kilometer moderately trafficked loop trail located near Sete Cidades, Azores, Portugal that features a lake and is rated as moderate. The trail is primarily used for hiking and mountain biking.",
+        "web":"https://www.alltrails.com/trail/portugal/azores/lagoa-verde-sete-cidades-e-lagoa-de-santiago"
       },
       "geometry": {
         "type": "Point",
@@ -21,7 +23,9 @@ var azores={
       "properties": {
         "name": "Ilha de Sao Miguel",
         "location": "Lagoa das Sete Cidases",
-        "pic": "https://portuguesecooking.files.wordpress.com/2015/03/lagoa-das-sete-cidades.jpg?w=816&h=9999"
+        "pic": "https://portuguesecooking.files.wordpress.com/2015/03/lagoa-das-sete-cidades.jpg",
+        "caption":"Lagoa das Sete Cidades is a twin lake situated in the crater of a dormant volcano on the Portuguese archipelago of the Azores. It consists of two small, ecologically different lakes connected by a narrow strait, which is crossed by a bridge. The volcano is located on the western third of the island of São Miguel",
+        "web":"https://en.wikipedia.org/wiki/Lagoa_das_Sete_Cidades"
       },
       "geometry": {
         "type": "Point",
@@ -36,7 +40,9 @@ var azores={
       "properties": {
         "name": "Ilha de Sao Miguel (Sao Miguel Island)",
         "location": "Lagoa do Fogo",
-        "pic": "https://experitour.com/wp-content/uploads/2019/02/lagoa-do-fogo-1-768x512.jpg"
+        "pic": "https://experitour.com/wp-content/uploads/2019/02/lagoa-do-fogo-1-768x512.jpg",
+        "caption":"Lagoa do Fogo is a crater lake within the Água de Pau Massif stratovolcano in the center of the island of São Miguel in the Portuguese archipelago of the Azores",
+        "web":"https://en.wikipedia.org/wiki/Lagoa_do_Fogo"
       },
       "geometry": {
         "type": "Point",
@@ -51,7 +57,9 @@ var azores={
       "properties": {
         "name": "Ilha do Faial (Faial Island)",
         "location": "Cabeço Gordo",
-        "pic": "https://farm6.staticflickr.com/5611/15582893202_8b8568ae79_b.jpg"
+        "pic": "https://farm6.staticflickr.com/5611/15582893202_8b8568ae79_b.jpg",
+        "caption":"Cabeço Gordo is the highest mountain, massive stratovolcano and the largest geomorphological structure that forms the island of Faial. The mountain/volcano is 1,043 meters above sea level, consisting of a two kilometer wide caldera, that is 400 m in depth below the crater rim",
+        "web":"https://en.wikipedia.org/wiki/Cabe%C3%A7o_Gordo"
       },
       "geometry": {
         "type": "Point",
@@ -66,7 +74,9 @@ var azores={
       "properties": {
         "name": "Ilha do Pico (Pico Island)",
         "location": "Reserva Natural da Montanha do Pico",
-        "pic": "https://labirintoperpetuo.files.wordpress.com/2011/08/pico.jpg"
+        "pic": "https://labirintoperpetuo.files.wordpress.com/2011/08/pico.jpg",
+        "caption":"Mount Pico is a stratovolcano located on Pico Island, in the mid-Atlantic archipelago of the Azores. It is the highest mountain of Portugal, at 2,351 metres above sea level, and is one of the highest Atlantic mountains; it is more than twice the elevation of any other peak in the Azores",
+        "web":"https://en.wikipedia.org/wiki/Mount_Pico"
       },
       "geometry": {
         "type": "Point",
