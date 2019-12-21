e44e7749235c28a43e3e1cc15c90276928fcc785
joel <joel_congo@hotmail.com>
2019-12-21 11:21:56 -0400

    more attribute added to the file

--- data/azoresIsland.js
+++ data/azoresIsland.js
@@ -1,80 +1,80 @@
 var azores={
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Ilha de Sao Miguel",
-          "location": "Lagoa do Santiago",
-          "pic": "https://byacores.com/wp-content/uploads/2017/10/DSC08576.jpg"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -25.773839950561523,
-            37.848968055726274
-          ]
-        }
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ilha de Sao Miguel",
+        "location": "Lagoa do Santiago",
+        "pic": "https://byacores.com/wp-content/uploads/2017/10/DSC08576.jpg"
       },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Ilha de Sao Miguel",
-          "location": "Lagoa das Sete Cidases",
-          "pic": "https://portuguesecooking.files.wordpress.com/2015/03/lagoa-das-sete-cidades.jpg?w=816&h=9999"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -25.79808712005615,
-            37.861877979726614
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Ilha de Sao Miguel",
-          "location": "Lagoa do Fogo",
-          "pic": "https://experitour.com/wp-content/uploads/2019/02/lagoa-do-fogo-1-768x512.jpg"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -25.476307868957516,
-            37.762097740097126
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Ilha do Pico",
-          "location": "Reserva Natural da Montanha do Pico",
-          "pic": "https://labirintoperpetuo.files.wordpress.com/2011/08/pico.jpg"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-           -28.400774002075195,
-            38.46817290162771
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Ilha do Faial",
-          "location": "Cabeço Gordo",
-          "pic": "https://farm6.staticflickr.com/5611/15582893202_8b8568ae79_b.jpg"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -28.715944290161133,
-            38.586484641562976
-          ]
-        }
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -25.773839950561523,
+          37.848968055726274
+        ]
       }
-    ]
-  }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ilha de Sao Miguel",
+        "location": "Lagoa das Sete Cidases",
+        "pic": "https://portuguesecooking.files.wordpress.com/2015/03/lagoa-das-sete-cidades.jpg?w=816&h=9999"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -25.79808712005615,
+          37.861877979726614
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ilha de Sao Miguel (Sao Miguel Island)",
+        "location": "Lagoa do Fogo",
+        "pic": "https://experitour.com/wp-content/uploads/2019/02/lagoa-do-fogo-1-768x512.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -25.476307868957516,
+          37.762097740097126
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ilha do Faial (Faial Island)",
+        "location": "Cabeço Gordo",
+        "pic": "https://farm6.staticflickr.com/5611/15582893202_8b8568ae79_b.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -28.715944290161133,
+          38.586484641562976
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ilha do Pico (Pico Island)",
+        "location": "Reserva Natural da Montanha do Pico",
+        "pic": "https://labirintoperpetuo.files.wordpress.com/2011/08/pico.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -28.400344848632812,
+          38.46810570012969
+        ]
+      }
+    }
+  ]
+}
