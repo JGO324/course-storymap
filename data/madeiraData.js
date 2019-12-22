07ded8e022b4200f57838dade976a10e45a35e6a
joel <joel_congo@hotmail.com>
2019-12-21 20:42:53 -0400

    data for madeira

---
+++ data/madeiraData.js
@@ -0,0 +1,69 @@
+var madeira={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "state": "Madeira",
+          "location": "Santa Cruz",
+          "capital": false,
+          "icon": "",
+          "image": "https://static.traveltek.net/uploaded/2015/4/1429271396.jpg",
+          "pop": 43005,
+          "cat": "city",
+          "caption": "",
+          "web": "http://mbest.wowsystems.pt/guide/facts-about/madeira-island-towns-and-municipalities/santa-cruz"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -16.82779312133789,
+            32.67174887226337
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "state": "Madeira",
+          "location": "Câmara de Lobos",
+          "capital": false,
+          "icon": "",
+          "image": "http://madeiratop10.com/img/uploads/767x578_4a083f78dc36bb4a06b8e574ed762a9e.jpg",
+          "pop": 35666,
+          "cat": "city",
+          "caption": "",
+          "web": "http://www.madeiratop10.com/en/pages/camara-de-lobos-madeira"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -17.0062457,
+            32.6601177
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "state": "Madeira",
+          "location": "Funchal",
+          "capital": true,
+          "icon": "",
+          "image": "http://3.bp.blogspot.com/-uSF_7p4_KmQ/UfqBql3ShqI/AAAAAAAACb0/0qhuePqWBSU/s400/Funchal+1.jpg",
+          "pop": 35666,
+          "cat": "city",
+          "caption": "",
+          "web": "http://www.madeiratop10.com/en/pages/madeira-funchal"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+           -16.915168762207028,
+            32.648047629886634
+          ]
+        }
+      }
+      
+    ]
+  }
